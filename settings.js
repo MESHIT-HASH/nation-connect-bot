f1158796c9cc6f2beac02a6c3813a04e06fb1f50
MESHIT-HASH <petermeshackomari@gmail.com>
2025-08-25 22:19:31 +0300

    Initial commit - Nation Connect Bot by DormantElement

---
+++ settings.js
@@ -0,0 +1,14 @@
+const settings = {
+  packname: 'Nation Hub Bot',
+  author: 'DormantElement',
+  botName: "Nation Connect Bot",
+  botOwner: 'DormantElement', // Your name or alias
+  ownerNumber: '254721443698', // Your WhatsApp number (country code + number, no + or spaces)
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq', // You can keep this or replace with your own
+  commandMode: "public", // "private" if you want only you to trigger commands
+  description: "Official bot for Nation Connect Hub - keeping the group safe, clean & engaging.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
+
